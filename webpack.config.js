add0309090d741817813d65239c0551bedd4ec92
Emmanuel Oppong-Sarpong <esarpong51@gmail.com>
2019-04-24 08:02:05 +0000

    bakup for version change

--- webpack.config.js
+++ webpack.config.js
@@ -23,18 +23,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js?$/g,
+                test: /\.js$/g,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env'],
-                    plugins: [
-                        ["transform-class-properties", { "spec": true }]
-                        //[require("@babel/plugin-proposal-class-properties"), {loose: true}]
-                    ]
-                },
+                include: /src/,
+                loader: 'babel-loader'
             },
-        ]
+        ],
     }
 
 };
